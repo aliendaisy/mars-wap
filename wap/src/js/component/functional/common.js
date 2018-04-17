0d8505ba4e47e36e324530da506f5a5273c912b8
alien <alien@yulingjiedeMacBook-Pro.local>
2018-04-17 23:40:41 +0800

    add fetch and carousel

---
+++ wap/src/js/component/functional/common.js
@@ -0,0 +1,21 @@
+import 'whatwg-fetch';
+
+//fetch通信  POST通信
+export function fetchJson(url,params,cb){
+    let myHeaders = new Headers({"Content-Type": "application/json"});
+    let headerUrl = 'http://www.marstail.com:20000/v1';
+    let allUrl = headerUrl + url;
+    fetch(allUrl,{
+        method: 'post',
+        headers: myHeaders,
+        mode: 'cors',
+        credentials: 'include',
+        body: JSON.stringify({params})
+    }).then(res => {
+        res.json().then(function(data){
+            return cb(data);
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+}
--- wap/src/js/component/pages/home.js
+++ wap/src/js/component/pages/home.js
@@ -5,6 +5,7 @@ import React,{Component} from 'react';
 import Header from '../items/header';
 
 import { Carousel } from 'antd-mobile';
+import {fetchJson} from "../functional/common";
 
 
 class Home extends Component{
@@ -12,7 +13,14 @@ class Home extends Component{
         super(props);
         this.state = {
             data: ['AiyWuByWklrrUDlFignR', 'TekJlZRVCjLFexlOCuWn', 'IJOtIlfsYdTyaDTRVrLI'],
+            week: ['Tue','Wed','Thu','Fri','Sat','Sun','Mon']
         }
+    }
+    componentDidMount() {
+        fetchJson('/user/getEventType',{},msg => {
+            console.log(msg);
+        });
+
     }
     render() {
         return(
@@ -28,9 +36,30 @@ class Home extends Component{
                     {this.state.data.map(val => {
                         return(
                             <a href="##" key={val}>
-                                <img src={`https://zos.alipayobjects.com/rmsportal/${val}.png`} alt=""/>
+                                <img
+                                    src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
+                                    alt=""
+                                    onLoad={() => {
+                                        // fire window resize event to change height
+                                        window.dispatchEvent(new Event('resize'));
+                                        this.setState({ imgHeight: 'auto' });
+                                    }}
+                                />
                             </a>
                     )})}
+                </Carousel>
+
+                <Carousel
+                    className="week-carousel"
+                    dots={false}
+                    autoplay={false}
+                >
+                    {this.state.week.map(val => {
+                        return(
+                            <p>{val}</p>
+                        )
+                    })}
+
                 </Carousel>
             </div>
         )
